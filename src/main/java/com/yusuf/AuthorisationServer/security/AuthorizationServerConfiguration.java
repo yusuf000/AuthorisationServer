9d69ee86b1dbe57cba429f6bead162a89fab61a3
Yusuf <md.yusuf@tigerit>
2023-10-10 13:25:40 +0600

    add resourcesSeverCheck

--- src/main/java/com/yusuf/AuthorisationServer/security/AuthorizationServerConfiguration.java
+++ src/main/java/com/yusuf/AuthorisationServer/security/AuthorizationServerConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -66,8 +67,7 @@ public class AuthorizationServerConfiguration {
                         .accessTokenResponseHandler(new CustomAccessTokenResponseHandler()))
                 .oidc(Customizer.withDefaults());
 
-        return http.oauth2ResourceServer((oauth2) -> oauth2
-                .jwt(Customizer.withDefaults())).build();
+        return http.build();
     }
 
     @Bean
--- src/main/java/com/yusuf/AuthorisationServer/security/SpringSecurityConfiguration.java
+++ src/main/java/com/yusuf/AuthorisationServer/security/SpringSecurityConfiguration.java
@@ -31,7 +31,8 @@ public class SpringSecurityConfiguration {
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
                 .httpBasic(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
-        return http.build();
+        return http.oauth2ResourceServer((oauth2) -> oauth2
+                .jwt(Customizer.withDefaults())).build();
 
     }
 
